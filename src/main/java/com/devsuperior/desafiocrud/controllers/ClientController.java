e1cb24f8f083a55c83ce44ede9c8ab66ce79b3b9
Douglas Fabiano <douglasfabiano23@gmail.com>
2022-12-11 17:13:55 -0300

    GET Clients paged

--- src/main/java/com/devsuperior/desafiocrud/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafiocrud/controllers/ClientController.java
@@ -1,16 +1,16 @@
 package com.devsuperior.desafiocrud.controllers;
 
 import com.devsuperior.desafiocrud.dto.ClientDTO;
-import com.devsuperior.desafiocrud.entities.Client;
-import com.devsuperior.desafiocrud.repositories.ClientRepository;
 import com.devsuperior.desafiocrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -21,5 +21,10 @@ public class ClientController {
     public ClientDTO findById(@PathVariable Long id){
     ClientDTO dto = service.findById(id);
     return dto;
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
+++ src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
@@ -4,9 +4,12 @@ import com.devsuperior.desafiocrud.dto.ClientDTO;
 import com.devsuperior.desafiocrud.entities.Client;
 import com.devsuperior.desafiocrud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +23,11 @@ public class ClientService {
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
